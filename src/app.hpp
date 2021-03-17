b84dbf1d156d4d8ca4e5a4d0c4a318bf7ea62ead
Otávio S. Gomes <otaviosantosg@gmail.com>
2021-03-17 11:22:41 -0300

    add menu options

--- src/app.hpp
+++ src/app.hpp
@@ -11,8 +11,16 @@ class App {
     
     void waitForKeyPressed();
     void displayMenuOptions();
-    void displayGraphOrder();
+
     void initGraphFromTxt();
+    void displayGraphOrder();
+    void displayGraphSize();
+    
+    void displayVertexDegree();
+
+
+    void isArticulation();
+    void isBridge();
 
     public:
     void run();
@@ -29,20 +37,57 @@ void App::displayMenuOptions() {
     cout << "Menu:" << endl;
     cout << "1 - Inicializar Grafo" << endl;
     cout << "2 - Ordem do Grafo" << endl;
+    cout << "3 - Tamanho do Grafo" << endl;
+    cout << "4 - Vizinhos de um vertice" << endl;
+    cout << "5 - Grau de um vertice" << endl;
+    cout << "6 - Busca em profundidade" << endl;
+    cout << "7 - Numero e vertices de componentes conexas" << endl;
+    cout << "8 - Verificar se vertice eh articulacao" << endl;
+    cout << "9 - Verificar se aresta eh ponte" << endl;
+    cout << "10 - Execucao automatica" << endl; //Para o teste da biblioteca faça um programa principal que leia o arquivo texto e salve em um arquivo texto as diversas informações sobre o grafo lido.
+    cout << "0 - Sair" << endl;
     cout << ": ";
-}
-
-void App::displayGraphOrder() {
-    cout << endl << "A ordem do grafo eh: " << graph.order() << endl << endl;
 }
 
 void App::initGraphFromTxt() {
 
 }
 
+void App::displayGraphOrder() {
+    cout << endl << "A ordem do grafo eh: " << graph.order() << endl << endl;
+}
+
+void App::displayGraphSize() {
+    cout << endl << "O tamanho do grafo eh: " << graph.size() << endl << endl;
+}
+
+void App::displayVertexDegree() {
+    int vertex;
+    cout << "Determinar grau de qual vertice? ";
+    cin >> vertex;
+    cout << endl << "O grau do vertice eh: " << graph.degree(vertex) << endl << endl;
+}
+
+void App::isArticulation() {
+    int vertex;
+    cout << "Vertice a ser analisado: ";
+    cin >> vertex;
+    if(graph.articulation(vertex)) cout << endl << "O vertice eh articulacao!" << endl << endl;
+    else                           cout << endl << "O vertice nao eh articulacao!" << endl << endl;
+}
+
+void App::isBridge() {
+    int to, from;
+    cout << "Aresta a ser analisada: (from, to) ";
+    cin >> from >> to;
+    Graph::Edge edge(from, to);
+    if(graph.bridge(edge)) cout << endl << "A aresta eh ponte!" << endl << endl;
+    else                   cout << endl << "A aresta nao eh ponte!" << endl << endl;
+}
+
 void App::run() {
-    while(1) {
-        int option;
+    int option = -1;
+    while(option != 0) {
         displayMenuOptions();
         cin >> option;
 
@@ -56,10 +101,42 @@ void App::run() {
             displayGraphOrder();
             break;
         
+        case 3:
+            displayGraphSize();
+            break;
+        
+        case 4:
+            break;
+        
+        case 5:
+            displayVertexDegree();
+            break;
+        
+        case 6:
+            break;
+        
+        case 7:
+            break;
+        
+        case 8:
+            isArticulation();
+            break;
+        
+        case 9:
+            isBridge();
+            break;
+        
+        case 10:
+            break;
+        
+        case 0:
+            cout << endl << "Sair" << endl << endl;
+            break;
+        
         default:
             break;
         }
-        waitForKeyPressed();
+        //waitForKeyPressed();
     }
 }
 
