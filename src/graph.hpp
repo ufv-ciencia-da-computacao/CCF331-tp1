227cec8605134d20a9cd10dfa69d2b8fd55db416
Germano Barcelos <gegenbarcelos@gmail.com>
2021-04-20 09:19:51 -0300

    test concluded alg3-opt

--- src/graph.hpp
+++ src/graph.hpp
@@ -47,7 +47,6 @@ class Graph {
     bool bridge(Edge edge);
     vector<int> closestNeighborHeuristic(int vertex);
 
-    int gainFrom3Opt(int x1, int x2, int y1, int y2, int z1, int z2, int optcase);
     void alg3opt(vector<int>& order);
 
     private:
@@ -282,7 +281,6 @@ void reverseSegment(vector<int>& order, int startIndex, int endIndex) {
     int inversionSize = ((N + endIndex - startIndex + 1) % N)/2;
 
     left = startIndex; right = endIndex;
-
     for (int i = 1; i <= inversionSize; i++) {
         int aux = order[left];
         order[left] = order[right];
@@ -293,7 +291,7 @@ void reverseSegment(vector<int>& order, int startIndex, int endIndex) {
     }  
 }
 
-int Graph::gainFrom3Opt(int x1, int x2, int y1, int y2, int z1, int z2, int optcase) {
+int gainFrom3Opt(int x1, int x2, int y1, int y2, int z1, int z2, int optcase, vector<vector<int>> matEdges) {
     int deleteLength, addLength;
 
     switch (optcase) {
@@ -302,32 +300,32 @@ int Graph::gainFrom3Opt(int x1, int x2, int y1, int y2, int z1, int z2, int optc
             addLength = 0;
             break;
         case 1:
-            deleteLength = adj[x1][x2].weight + adj[z1][z2].weight;
-            addLength = adj[x1][z1].weight + adj[x2][z2].weight;
+            deleteLength = matEdges[x1][x2] + matEdges[z1][z2];
+            addLength = matEdges[x1][z1] + matEdges[x2][z2];
             break;
         case 2:
-            deleteLength = adj[y1][y2].weight + adj[z1][z2].weight;
-            addLength = adj[y1][z1].weight + adj[y2][z2].weight;
+            deleteLength = matEdges[y1][y2] + matEdges[z1][z2];
+            addLength = matEdges[y1][z1] + matEdges[y2][z2];
             break;
         case 3:
-            deleteLength = adj[x1][x2].weight + adj[y1][y2].weight;
-            addLength = adj[x1][y1].weight + adj[x2][y2].weight;
+            deleteLength = matEdges[x1][x2] + matEdges[y1][y2];
+            addLength = matEdges[x1][y1] + matEdges[x2][y2];
             break;
         case 4:
-            deleteLength = adj[x1][x2].weight + adj[y1][y2].weight + adj[z1][z2].weight;
-            addLength = adj[x1][y1].weight + adj[x2][z2].weight + adj[y2][z1].weight;
+            deleteLength = matEdges[x1][x2] + matEdges[y1][y2] + matEdges[z1][z2];
+            addLength = matEdges[x1][y1] + matEdges[x2][z1] + matEdges[y2][z2];
             break;
         case 5:
-            deleteLength = adj[x1][x2].weight + adj[y1][y2].weight + adj[z1][z2].weight;
-            addLength = adj[x1][z1].weight + adj[y2][x2].weight + adj[y1][z2].weight;
+            deleteLength = matEdges[x1][x2] + matEdges[y1][y2] + matEdges[z1][z2];
+            addLength = matEdges[x1][z1] + matEdges[y2][x2] + matEdges[y1][z2];
             break;
         case 6:
-            deleteLength = adj[x1][x2].weight + adj[y1][y2].weight + adj[z1][z2].weight;
-            addLength = adj[x1][y2].weight + adj[z1][y1].weight + adj[x2][z2].weight;
+            deleteLength = matEdges[x1][x2] + matEdges[y1][y2] + matEdges[z1][z2];
+            addLength = matEdges[x1][y2] + matEdges[z1][y1] + matEdges[x2][z2];
             break;
         case 7:
-            deleteLength = adj[x1][x2].weight + adj[y1][y2].weight + adj[z1][z2].weight;
-            addLength = adj[x1][y2].weight + adj[z1][x2].weight + adj[y1][z2].weight;
+            deleteLength = matEdges[x1][x2] + matEdges[y1][y2] + matEdges[z1][z2];
+            addLength = matEdges[x1][y2] + matEdges[z1][x2] + matEdges[y1][z2];
             break;
     }
 
@@ -348,8 +346,8 @@ void make3OptMove(vector<int>& order, int i, int j, int k, int optcase) {
             reverseSegment(order, (i+1)%N, j);
             break;
         case 4:
-            reverseSegment(order, (i+1)%N, j);
             reverseSegment(order, (j+1)%N, k);
+            reverseSegment(order, (i+1)%N, j);
             break;
         case 5:
             reverseSegment(order, (k+1)%N, i);
@@ -368,34 +366,42 @@ void make3OptMove(vector<int>& order, int i, int j, int k, int optcase) {
 }
 
 void Graph::alg3opt(vector<int>& order) {
-    int N = order.size();
     bool locallyOptimal = false;
     int i, j, k;
     int x1, x2, y1, y2, z1, z2;
-    int optCase;
+    int optCase[7] = {1, 2, 3, 4, 5, 6, 7};
     int moveGain;
+    
+    vector<vector<int>> matEdges(N+1, vector<int>(N+1));
+    vector<Adjacent> list;
+
+    for(int i=1; i<=N; i++) {
+        list = adj[i];
+        for(int j=0; j<list.size(); j++) {
+            matEdges[i][list[j].to] = list[j].weight;
+        }
+    }
 
     while (!locallyOptimal) {
         locallyOptimal = true;
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i <= N-1; i++) {
             x1 = order[i];
-            x2 = order[i+1%N];
+            x2 = order[(i+1)%N];
 
-            for (int counter2 = 1; counter2 < N-2; counter2++) {
+            for (int counter2 = 1; counter2 <= N-3; counter2++) {
                 j = (i+counter2)%N;
                 y1 = order[j];
-                y1 = order[(j+1)%N];
+                y2 = order[(j+1)%N];
 
-                for (int counter3 = counter2+1; counter3 < N; counter3++) {
+                for (int counter3 = counter2+1; counter3 <= N-1; counter3++) {
                     k = (i+counter3) %N;
                     z1 = order[k];
-                    z1 = order[(k+1) %N];
+                    z2 = order[(k+1) %N];
 
-                    for (int optCase = 1; optCase < 8; optCase++) {
-                        moveGain = gainFrom3Opt(x1, x2, y1, y2, z1, z2, optCase);
-
+                    for (int l = 0; l < 6; l++) {
+                        moveGain = gainFrom3Opt(x1, x2, y1, y2, z1, z2, optCase[l], matEdges);
                         if (moveGain > 0) {
-                            make3OptMove(order, i, j, k, optCase);
+                            make3OptMove(order, i, j, k, optCase[l]);
                             locallyOptimal = false;
                             break;
                         }
--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,28 @@
 
 int main() {
 
-    vector<Graph::Edge> edgeList;
-    edgeList.emplace_back(1, 2, 1);
-    edgeList.emplace_back(1, 3, 2);
-    edgeList.emplace_back(1, 4, 3);
-    edgeList.emplace_back(2, 3, 7);
-    edgeList.emplace_back(2, 4, 1);
-    edgeList.emplace_back(3, 4, 4);
-    Graph g(edgeList, 4);
+    // vector<Graph::Edge> edgeList;
+    // edgeList.emplace_back(1, 2, 1);
+    // edgeList.emplace_back(1, 3, 2);
+    // edgeList.emplace_back(1, 4, 3);
+    // edgeList.emplace_back(2, 3, 7);
+    // edgeList.emplace_back(2, 4, 1);
+    // edgeList.emplace_back(3, 4, 4);
+    // Graph g(edgeList, 4);
 
-    for(int x : g.closestNeighborHeuristic(1)) {
-        cout << x << " ";
-    }
-    cout << endl;
+    // vector<int> order = g.closestNeighborHeuristic(1);
+
+    // for(int x : order) {
+    //     cout << x << " ";
+    // }
+    // cout << endl;
+
+    // g.alg3opt(order);
+
+    // for(int x : order) {
+    //     cout << x << " ";
+    // }
+    // cout << endl;
 
     // App app;
     // app.run();
